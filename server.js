aba01300d278a1d03db6cf6b2e7c679bf878d48a
boudou Nathan <anticamp2@gmail.com>
2020-07-09 18:27:53 +0200

    change bootstrap version

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname+'/src/home/home.component.html'));
 });
 
 // Start the app by listening on the default Heroku port
