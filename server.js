2a417fbc3ab9526845e4ae81c345ea012e6919f1
boudou Nathan <anticamp2@gmail.com>
2020-07-09 14:32:30 +0200

    change index path

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<Bet-es>'));
+app.use(express.static(__dirname + '/dist/Bet-es_Front'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<Bet-es>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Bet-es_Front/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
